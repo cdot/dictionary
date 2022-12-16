70111a232cb7a4db3927d3b095a8ae67525256b1
cdot <github@c-dot.co.uk>
2022-12-16 12:24:51 +0000

    Forgot these

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { Dictionary } from "./Dictionary.js";
+import { Explorer } from "./Explorer.js";
+import { LetterNode } from "./LetterNode.js";
+import { TrieNode } from "./TrieNode.js";
+import { Trie } from "./Trie.js";
+
+export {
+  Dictionary,
+  Explorer,
+  LetterNode,
+  TrieNode,
+  Trie
+}
